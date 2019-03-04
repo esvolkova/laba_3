77a3afddb3cfd890eb3a0b2519efbc2bded3b020
esvolkova <48125195+esvolkova@users.noreply.github.com>
2019-03-04 20:15:44 +0300

    Add files via upload

---
+++ laba_3_part1.py
@@ -0,0 +1,78 @@
+#This code gets inf about PTZ camera and its Absolute move in command line
+
+from onvif import ONVIFCamera
+
+# Connecting
+mycam = ONVIFCamera('192.168.15.42', 80, 'esvolkova5', 'UF8OMwhzaG07', 'C:/Python27/Lib/site-packages/onvif/wsdl')
+
+# Getting information
+print mycam.devicemgmt.GetDeviceInformation()
+
+# Creating media service
+media_service = mycam.create_media_service()
+
+# Getting profiles
+profiles = media_service.GetProfiles()
+media_profile = profiles[0]
+print 'Profiles: ', media_profile
+
+# Getting token
+token = media_profile._token
+print 'Token: ', token
+
+# Create ptz service object
+ptz = mycam.create_ptz_service()
+
+#Get available PTZ services
+request = ptz.create_type('GetServiceCapabilities')
+Service_Capabilities = ptz.GetServiceCapabilities(request)
+print 'PTZ service capabilities:', Service_Capabilities
+
+#Get PTZ status
+status = ptz.GetStatus({'ProfileToken':token})
+
+#Current coordinates
+print 'PTZ status:', status
+print 'Pan position:', status.Position.PanTilt._x
+print 'Tilt position:', status.Position.PanTilt._y
+print 'Zoom position:', status.Position.Zoom._x
+
+# Get PTZ configuration options for getting option ranges
+request = ptz.create_type('GetConfigurationOptions')
+request.ConfigurationToken = media_profile.PTZConfiguration._token
+ptz_configuration_options = ptz.GetConfigurationOptions(request)
+print 'PTZ configuration options:', ptz_configuration_options
+
+
+###
+
+#Absolute move
+request_absolute_move = ptz.create_type('AbsoluteMove')
+request_absolute_move.ProfileToken = media_profile._token
+print 'Absolute move options: ', request_absolute_move
+
+#Absolute pan-tilt (pan position, tilt position, velocity)
+# position 1
+pan = -0.4
+tilt = -0.4
+velocity = 0.4
+
+# position 2
+pan = 0.2
+tilt = -0.2
+velocity = 0.7
+
+request_absolute_move.Position.PanTilt._x = pan
+request_absolute_move.Position.PanTilt._y = tilt
+request_absolute_move.Speed.PanTilt._x = velocity
+request_absolute_move.Speed.PanTilt._y = velocity
+
+print 'Absolute move to:', request_absolute_move.Position
+print 'Absolute speed:',request_absolute_move.Speed
+# Moving
+ptz.AbsoluteMove(request_absolute_move)
+print 'Absolute move pan-tilt requested:', pan, tilt, velocity
+#Get current position
+status = ptz.GetStatus({'ProfileToken':token})
+#Show Current coordinates
+print 'PTZ status:', status
